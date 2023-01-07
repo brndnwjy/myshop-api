f57a5fa71820478cb5cc2b544d20120eb6cf8f42
Brandon Wijaya <brndnwjy8@gmail.com>
2023-01-08 02:29:49 +0700

    Disabled firebase auth for deployment

--- src/router/cart.routes.js
+++ src/router/cart.routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { getAll, insert, remove } = require("../controller/cart.controller");
-const firebaseAuth = require("../middleware/auth-firebase");
+// const firebaseAuth = require("../middleware/auth-firebase");
 
 router
-  .get("/:uid", firebaseAuth, getAll)
-  .post("/", firebaseAuth, insert)
-  .delete("/:id", firebaseAuth, remove);
+  .get("/:uid", getAll)
+  .post("/", insert)
+  .delete("/:id", remove);
 
 module.exports = router;
